89d56e4462a05da027527cc6af76ccb9126f4add
salma saadaoui <salma.saadaoui1998@gmail.com>
2023-08-14 16:51:46 +0100

    test routine

--- input.c
+++ input.c
@@ -1,4 +1,4 @@
-/* ************************************************************************** */
+/******************************************************************************/
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   input.c                                            :+:      :+:    :+:   */
@@ -6,9 +6,9 @@
 /*   By: ssaadaou <ssaadaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/07 18:53:15 by ssaadaou          #+#    #+#             */
-/*   Updated: 2023/07/20 04:58:42 by ssaadaou         ###   ########.fr       */
+/*   Updated: 2023/08/14 16:44:21 by ssaadaou         ###   ########.fr       */
 /*                                                                            */
-/* ************************************************************************** */
+/******************************************************************************/
 
 #include "philo.h"
 
@@ -49,15 +49,74 @@ int valid_input(int ac, char **av , t_list *args)
     return(0);
 }
 
+long long time_()
+{
+    struct timeval time_var;
+    gettimeofday(&time_var, NULL);
+    long long now;
+    now = time_var.tv_sec * 1000 + time_var.tv_usec / 1000;
+    return(now);
+}
+void *routine(void *ph)
+{
+    t_philo *philo = (t_philo *)ph;
+    pthread_mutex_init(&philo->data->printf_, NULL);
+    pthread_mutex_lock(&philo->data->fork[philo->id - 1]);
+    pthread_mutex_lock(&philo->data->printf_);
+    printf("philo %d right fork\n", philo->id);
+    pthread_mutex_unlock(&philo->data->printf_);
+    pthread_mutex_lock(&philo->data->fork[philo->id % philo->data->num_philo]);
+    pthread_mutex_lock(&philo->data->printf_);
+    printf("philo %d left fork\n", philo->id);
+    pthread_mutex_unlock(&philo->data->printf_);
+    pthread_mutex_lock(&philo->data->printf_);
+    
+    printf("philo is eating\n");
+    pthread_mutex_unlock(&philo->data->printf_);
+    usleep(800);
+    pthread_mutex_unlock(&philo->data->fork[philo->id - 1]);
+    pthread_mutex_unlock(&philo->data->fork[philo->id % philo->data->num_philo]);
+    pthread_mutex_lock(&philo->data->printf_);    
+    printf("philo is thinking\n");
+    pthread_mutex_unlock(&philo->data->printf_);
+
+    // pthread_mutex_unlock(&philo->data->fork);
+    
+    return NULL;
+    
+}
+
+void init_fork(t_list *data)
+{
+    int i = 0;
+    while(i < data->num_philo)
+    {
+        pthread_mutex_init(&data->fork[i], NULL);
+        data->ph[i].data = data;
+        i++;
+    }
+}
+void init_philo(t_list *data)
+{
+    int i = 0; 
+    while(i < data->num_philo)
+    { 
+        data->ph[i].id = i + 1;
+        data->ph[i].last_meal = time_();
+	    pthread_create(&data->ph[i].philo, NULL, routine, &data->ph[i]);
+        // usleep(300);
+        pthread_detach(data->ph[i].philo);
+        i++;
+    }
+}
 int main(int ac, char **av)
 {
     t_list args;
     if(valid_input(ac, av, &args))
         return 1; 
-    // int i = 0;
-    printf("philo number total = %d\n", args.num_philo);
-    // while(i < args.num_philo)
-    // {
-        
-    // }
+    
+    args.fork = malloc(sizeof(pthread_mutex_t) * args.num_philo);
+    args.ph = malloc(sizeof(t_philo) * args.num_philo);
+    init_fork(&args);
+    init_philo(&args);
 }
--- philo.h
+++ philo.h
@@ -1,4 +1,4 @@
-/* ************************************************************************** */
+/******************************************************************************/
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   philo.h                                            :+:      :+:    :+:   */
@@ -6,9 +6,9 @@
 /*   By: ssaadaou <ssaadaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/07 19:28:30 by ssaadaou          #+#    #+#             */
-/*   Updated: 2023/07/20 04:52:59 by ssaadaou         ###   ########.fr       */
+/*   Updated: 2023/08/14 16:23:15 by ssaadaou         ###   ########.fr       */
 /*                                                                            */
-/* ************************************************************************** */
+/******************************************************************************/
 
 #ifndef PHILO_H
 #define PHILO_H
@@ -16,6 +16,16 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
+#include <sys/time.h>
+typedef struct s_philo
+{
+    int id;
+    pthread_t philo;
+    long long last_meal;
+    struct s_list *data;
+
+}   t_philo;
 
 typedef struct s_list
 {
@@ -24,13 +34,14 @@ typedef struct s_list
     int time_to_eat;
     int time_to_sleep;
     int num_of_meals;
+    pthread_mutex_t printf_;
+    pthread_mutex_t *fork;
+    t_philo *ph;
     
+
 }t_list;
 
-typedef struct s_philo
-{
-    
-}t_philo;
+
 //parsing
 
 int ft_atoi(char *s);
